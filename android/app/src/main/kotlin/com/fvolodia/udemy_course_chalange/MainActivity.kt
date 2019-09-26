f4569bb16daf9f5266e5ca5e6c551eacd0075fa1
vfaliuta <volodymyr.faliuta@globaltechmakers.com>
2019-09-26 21:00:46 +0300

    - init;

---
+++ android/app/src/main/kotlin/com/fvolodia/udemy_course_chalange/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fvolodia.udemy_course_chalange
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
